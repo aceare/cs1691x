3de95bf658b9d0beeaaeaae0807ab89c78d9c137
saasbook <saasbook@saasbook>
2012-10-13 00:39:12 -0700

    Rails - default project generated in eclipse

---
+++ rails1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails1::Application.config.secret_token = 'a8e84a1ca03370d6a2983f6d29cba5e89b607de73fe25bcbec315265414e44b09df7e048bfe37583649c1c4a681770926b2bc246c7978e51eaea3e161523166f'
